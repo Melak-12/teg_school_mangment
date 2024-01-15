3416fbfa945c26be73f1eba45f1fe4db3d8463a0
melak-12 <melakabebeee@gmail.com>
2024-01-15 15:31:49 +0300

    login page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
 // import Dashboard from "./dashboard/page";
-// import LoginPage from './auth/login/page.jsx'
+import LoginPage from './auth/login/page.jsx'
 export default function Home() {
   return (
     <main className="">
-      {/* <LoginPage/> */}fsd
+      <LoginPage/>
     </main>
   )
 }
